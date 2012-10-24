e074c1311aab6e3da6523416fef3a8fc751808cb
Marc Buils <marc.buils@gmail.com>
2012-10-24 15:08:56 +0200

    #1: Add a asynchron lazyload system for automatic script download

--- jquery.pluginautoload.js
+++ jquery.pluginautoload.js
@@ -11,6 +11,10 @@
  * v0.2.0:
  * Add Ajax automatic script import 
  * Add a better parmeters parser
+ * Add exemples
+ * 
+ * v0.3.0:
+ * Change Ajax loading system 
  */
 (function($){
 	$.pluginautoload_options = {
@@ -22,6 +26,34 @@
 		}
 	};
 	
+	// lazyload script
+	// ref: http://www.nczonline.net/blog/2009/07/28/the-best-way-to-load-external-javascript/
+	var _loadScript = function(url, callback){
+	
+	    var script = document.createElement("script")
+	    script.type = "text/javascript";
+	
+	    if (script.readyState){  //IE
+	        script.onreadystatechange = function(){
+	            if (script.readyState == "loaded" ||
+	                    script.readyState == "complete"){
+	                script.onreadystatechange = null;
+	                callback();
+	            }
+	        };
+	    } else {  //Others
+	        script.onload = function(){
+	            callback();
+	        };
+	    }
+	
+	    script.src = url;
+	    document.getElementsByTagName("head")[0].appendChild(script);
+	};
+
+	// Script loaded list
+	var _loaded = {};
+
 	$.fn.pluginautoload = function( p_options ){
 		var _options = $.extend({}, $.pluginautoload_options, p_options);
 		
@@ -30,21 +62,31 @@
 			var _params = ( typeof($_this.attr('data-jquery-params')) == "undefined" ? [] : JSON.parse($_this.attr('data-jquery-params')) );
 			var _type = $_this.attr('data-jquery-type');
 			
-			if (_options.autoimport && typeof($.fn[_type]) == "undefined"){
-				$.ajax( _options.filename( _type ), {
-					async:			false,
-					dataTypeString:	'script'
-				});
-				if ( typeof($.fn[_type]) != "undefined" ){
-					$_this[_type].apply($_this, _params);
-				} else {
-					console.error("File %s loaded but plugin $.fn.%s not found", _options.filename( _type ), _type );
+			if ( _options.autoimport && typeof($.fn[_type]) == "undefined" ){
+				if ( typeof( _loaded[ _type ] ) == "undefined" ){
+					var _deferred = $.Deferred();
+					_loaded[ _type ] = _deferred.promise();
+					_loadScript( _options.filename( _type ), function(){
+						if ( typeof($.fn[_type]) == "undefined" ){
+							if ( typeof(console) != "undefined" ){
+								console.error( "File %s loaded but plugin $.fn.%s not found", _options.filename( _type ), _type );
+							}
+						} else {
+							_deferred.resolve( );
+						}
+						_loaded[ _type ] = undefined;
+					});
 				}
+				_loaded[ _type ].done(function(){
+					$_this[_type].apply($_this, _params);
+				});
 			}else{
 				if ( typeof($.fn[_type]) != "undefined" ){
 					$_this[_type].apply($_this, _params);
 				} else {
-					console.error("Plugin $.fn.%s not found", _type );
+					if ( typeof(console) != "undefined" ){
+						console.error("Plugin $.fn.%s not found", _type );
+					}
 				}
 			}
 			$_this.addClass('jquery_pluginautoload');
